c82018d024c30998d807050a7264836432fe8444
anshulsrivastava21 <70945637+anshulsrivastava21@users.noreply.github.com>
2020-10-29 19:33:27 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -10,41 +10,42 @@ wall = createSprite(1200,200,thickness,height/2)
 }
 
 function draw() {
-thickness = random(22,83)
-speed = random(223,321)
-weigt = random(30,52)
-wall.shapeColor = color(80,80,80)
-}
+  background(0);  
+  //bullet.sprite.collide(wall.sprite,calculateDeformation)
+  if(hasCollided(bullet, wall))
+  {
+  	bullet.velocityX=0;
+  	var damage=0.5 * weight * speed* speed/(thickness *thickness *thickness);
 
-function hasCollided(lbullet, lwall){
-bulletRightEdge = lbullet.x +lbullet.width
-wallLeftEdge=lwall.x
+  	
+	if(damage>10)
+	{
+		wall.shapeColor=color(255,0,0);
+		
+	}
 
-if(bulletRightEdge>=wallLeftEdge){
-return true
-}
-return false
-}
+	
 
-if(hasCollided(bullet,wall)){
-
-bullet.velocityx=0
-var damage = 0.5 * weight * speed*  speed/ (thickness *thickness *thickness)
-
-|
-
-if(damage>10){
-
-  wall.shapeColor = color(255,0,0)
-
-}
+	if(damage<10)
+	{
+		wall.shapeColor=color(0,255,0);
+	}
+	
+  }
 
 
-if(damage>10){
-
-  wall.shapeColor = color(255,0,0)
-
+  drawSprites();
+ 
 }
 
 
+function hasCollided(lbullet, lwall)
+{
+	bulletRightEdge=lbullet.x +lbullet.width;
+	wallLeftEdge=lwall.x;
+	if (bulletRightEdge>=wallLeftEdge)
+	{
+		return true
+	}
+	return false;
 }
